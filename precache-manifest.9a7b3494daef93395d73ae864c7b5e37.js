7878f94b06b3afec03a491ae803ee3ec09d52b83
raj-charalingal <charalingal.rajan@gmail.com>
2020-05-02 04:33:57 +0530

    Updates

---
+++ precache-manifest.9a7b3494daef93395d73ae864c7b5e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce76d0035d770f988612ec927ac8149",
+    "url": "/my-react-based-resume-website/index.html"
+  },
+  {
+    "revision": "c8029c81cbc943afe3c3",
+    "url": "/my-react-based-resume-website/static/css/main.a8fc0199.chunk.css"
+  },
+  {
+    "revision": "c44fc0a25c78b3ae5b64",
+    "url": "/my-react-based-resume-website/static/js/2.e555a335.chunk.js"
+  },
+  {
+    "revision": "90f8df42b25649df7004ebf7bb2be3cf",
+    "url": "/my-react-based-resume-website/static/js/2.e555a335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8029c81cbc943afe3c3",
+    "url": "/my-react-based-resume-website/static/js/main.84fba0f8.chunk.js"
+  },
+  {
+    "revision": "579d31856da4def9be9f",
+    "url": "/my-react-based-resume-website/static/js/runtime-main.5fe6e521.js"
+  },
+  {
+    "revision": "7a64cb1aba0fe789d19d365858ffd21a",
+    "url": "/my-react-based-resume-website/static/media/bgimg.7a64cb1a.jpg"
+  }
+]);
